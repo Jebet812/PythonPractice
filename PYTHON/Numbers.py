dc8fe5159b003d01cd7e8bad62397217273b1a78
Jebet812 <cynthiajebet08@gmail.com>
2022-11-12 13:34:14 +0300

    Modified

--- PYTHON/Numbers.py
+++ PYTHON/Numbers.py
@@ -1,7 +1,34 @@
-def additionresult ():
-    print("\nAddition")
-    8+12
-  
-additionresult()  
+print("Integers")
 
-  
+def Integers():
+    print("\nAddition")
+    result = 8 + 12
+    print(result)
+    
+    print("\nSubrtaction")
+    result = 12 - 8
+    print(result)
+    
+    print("\nMultiplication")
+    result = 8 * 12
+    print(result)
+    
+    
+    print("\nDivision")
+    result = 12 / 4
+    print(result)
+    
+    
+    print("\nExponents")
+    result = 8 ** 12
+    print(result)
+    
+    
+    print("\nMixed Operation")
+    result = 2 + 4 * 6
+    print(result)
+    
+Integers()  
+
+print("Floats")  
+

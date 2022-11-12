17573ef83e3119ae19ae3c9f57c08752d9525752
Jebet812 <cynthiajebet08@gmail.com>
2022-11-12 12:38:35 +0300

    Python Practices

---
+++ PYTHON/Strings.py
@@ -0,0 +1,49 @@
+def  string():
+    print("\nSimple String")
+    Welcome_message = "Hello! Welcome Cynthia!"
+    print(Welcome_message)    
+
+string()
+
+def  capitalize():
+    print("\nChanging case in a string with methods")
+    name = "cynthia jebet"
+    print(name.title())
+    
+capitalize()
+
+def Alluppercase():
+    print("\nChange String to all uppercase")
+    name = "cynthia jebet"
+    print(name.upper())
+    
+Alluppercase()
+
+def Alllowecase():
+    print("\nChange String to all lowercase")
+    name = "cynthia jebet"
+    print(name.lower())
+    
+Alllowecase()
+
+def Concertenating():
+    print("\nConcatenating/Combining Strings")
+    first_name = "cynthia"
+    second_name = "jebet"
+    print("How are you" + " " + first_name.title() + " " + second_name.title() + "!")
+
+Concertenating()
+
+def Addtab_newline():
+    print("\nAdding a tab and a new line in a single string")
+    print("My favourite colours are: \n\tRed\n\tPurple\n\tSage")
+    
+Addtab_newline()
+
+ def Strippingwhitespace():
+     print("\nStripping whitespace")
+     
+     
+     
+    
+
---
+++ PYTHON/Variables.py
@@ -0,0 +1,16 @@
+def Welcome():
+    print("Variables")
+    greeting = "Welcome to Python Cynthia. You'll enjoy this!"
+    print(greeting)
+
+Welcome()
+
+def WelcomeMessage():
+    print("\nPrinting a Second Message on a New Line")
+    greeting = "Welcome to Python Cynthia. You'll enjoy this!"
+    print(greeting)   
+    
+    greeting = "It's quite fun to do!"
+    print(greeting) 
+
+WelcomeMessage() 

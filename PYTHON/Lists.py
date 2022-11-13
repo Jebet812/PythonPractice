26aeded877fe3b613d96689cb03dffd1704be5a1
Jebet812 <cynthiajebet08@gmail.com>
2022-11-13 07:36:26 +0300

    Modified

---
+++ PYTHON/Lists.py
@@ -0,0 +1,15 @@
+print("Lists")
+
+def Simplelist():
+    print("\na simple list")
+    fruits = ['mangoes', 'oranges', 'bananas', 'apples']
+    print(fruits)
+    
+Simplelist()
+
+def Elementsinalist():
+    print("\nAccessing elements in a List")
+    fruits = ['mangoes', 'oranges', 'bananas', 'apples']
+    print (fruits[1].title())
+    
+Elementsinalist()

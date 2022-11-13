1fb3f12b68ad48a5120d6cef2595589621537d65
Jebet812 <cynthiajebet08@gmail.com>
2022-11-13 08:15:52 +0300

    Modified

--- PYTHON/Lists.py
+++ PYTHON/Lists.py
@@ -20,3 +20,64 @@ def Indexingpositions():
     print(fruits[-1])
     
 Indexingpositions()
+
+def Individualvalues():
+    print("\nUsing individual values on a list")
+    fruits = ['Mangoes', 'Oranges', 'Bananas', 'Apples']
+    print( "My favourite fruit is"  +  " " + fruits[-1].title() + ".")
+    
+Individualvalues()
+
+
+print("Changing, Adding and Removing Elements")
+
+def Modifyelementsinalist():
+    print("\nModifying Elements in a List")
+    colours = ['Red', 'Purple', 'Green', 'Orange']
+    print(colours)
+    
+    colours[0] = 'Brown'
+    print(colours)
+    
+Modifyelementsinalist()
+
+def Addingelementsinalist():
+    print("\nAdding Elements in a List using append")
+    colours = ['Red', 'Purple', 'Green', 'Orange']
+    print(colours)
+    
+    colours.append('Brown')
+    print(colours)
+    
+Addingelementsinalist()
+
+def Addingelementsinanemptylist():
+    print("\nAdding Elements in an empty List using append")
+    colours = []
+    print(colours)
+    
+    colours.append('Pink')
+    colours.append('Yellow')
+    print(colours)
+    
+Addingelementsinanemptylist()
+
+def Addingelementsinalistusinginsert():
+    print("\nAdding Elements in a List using insert")
+    colours = ['Red', 'Purple', 'Green', 'Orange']
+    print(colours)
+    
+    colours.insert(1, 'Pink' )
+    print(colours)
+    
+Addingelementsinalistusinginsert()
+
+def Removingelementsinalist():
+    print("\nModifying Elements in a List")
+    colours = ['Red', 'Purple', 'Green', 'Orange']
+    print(colours)
+    
+    colours[0] = 'Brown'
+    print(colours)
+    
+Modifyelementsinalist()

c7e26d10121d83cf86f58113d61e80f28b2ce9ef
Jebet812 <cynthiajebet08@gmail.com>
2022-11-14 09:14:28 +0300

    Modified

--- PYTHON/Working_with_Lists.py
+++ PYTHON/Working_with_Lists.py
@@ -67,3 +67,90 @@ def Statistics():
 
     
 Statistics()
+
+
+print("\nWorking with Part of a List")
+
+def slicingalist():
+    print("\nGenerate a subset of a list")
+    boys = ['Charles', 'Michael', 'Timothy', 'Titus', 'Trevor']
+    print(boys[2:4])
+    
+
+    print("\nGenerate a subset of a list ommiting first index")
+    boys = ['Charles', 'Michael', 'Timothy', 'Titus', 'Trevor']
+    print(boys[:4])
+    
+    print("\nGenerate a subset of a list ommiting last index")
+    boys = ['Charles', 'Michael', 'Timothy', 'Titus', 'Trevor']
+    print(boys[2:])
+    
+    
+    print("\nGenerate the last 3 subset of a list")
+    boys = ['Charles', 'Michael', 'Timothy', 'Titus', 'Trevor']
+    print(boys[-3:])
+    
+
+slicingalist()
+
+def loopingthroughalist():
+    print("\nLooping through a list")
+    boys = ['Charles', 'Michael', 'Timothy', 'Titus', 'Trevor']
+    print("\nThese three boys scored top in math")
+    for boy in boys[:3]:
+        print(boy.title())
+        
+loopingthroughalist()
+
+
+def copyingalist():
+    print("\nCopying a List")
+    snacks = ['Crackers', 'Crisps', 'Pizza']
+    duez_snacks = snacks[:]
+    
+    snacks.append('Soda')
+    duez_snacks.append('Ice cream')
+    
+    print("The snacks I carried are:")
+    print(snacks)
+    
+    print("\nThe snacks my best friend carried are:")
+    print(duez_snacks)
+    
+copyingalist()
+
+
+print("\nTuples")
+
+def Simpletuple():
+    print("\nSimple Tuple")
+    dimension = (100,20)
+    print(dimension[0])
+    print(dimension[1])
+    
+    
+Simpletuple()
+
+
+def Loopingintuple():
+    print("\nLooping through All Values in Tuple")
+    dimensions = (100,20)
+    for dimension in dimensions:
+         print(dimension)
+
+Loopingintuple()
+
+def Writingoveratuple():
+    print("\nWriting over a Tuple")
+    dimensions = (100,20)
+    print("\nOriginal Dimensions")
+    for dimension in dimensions:
+         print(dimension)
+         
+     
+    dimensions = (100,20)
+    print("\nChanged Dimensions")
+    for dimension in dimensions:
+         print(dimension)  
+         
+Writingoveratuple()  

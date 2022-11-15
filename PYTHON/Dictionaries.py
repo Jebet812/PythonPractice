8cd86c61a684fa2dc3d4b261ccd57f6dca3334ef
Jebet812 <cynthiajebet08@gmail.com>
2022-11-15 09:30:19 +0300

    Modified

--- PYTHON/Dictionaries.py
+++ PYTHON/Dictionaries.py
@@ -38,4 +38,48 @@ addtoempty()
 
 def modifyvalues():
     print("\nModifying Values in a Dictionary")
+    girl_1 = {'hair' : 'ginger'}
+    print("The girl's hair colour is" + " " + girl_1['hair'])
+    
+    girl_1 = {'hair' : 'brown'}
+    print("The girl's hair colour changed to" + " " + girl_1['hair'])
+    
+modifyvalues()
+
+def removekeyvaluepairs():
+    print("\nRemoving Key-Value Pairs")
+    girl_1 = {'eyes' : 'blue', 'hair' : 'ginger'}
+    del girl_1['hair']
+    print(girl_1)
+    
+removekeyvaluepairs()
+          
+def similarobjects():
+    print("\nA dictionary of similar objects")
+    favourite_car = {
+        'Jay' : 'Benz',
+        'Jane': 'bmw',
+        'Jason' : 'Audi'
+    }
+    print("Janes favourite car is a" + " " + 
+          favourite_car['Jane'].upper())
+    
+similarobjects()
+
+#Looping Through a Dictionary
+def loopingthroughkeyvalue():
+    print("\nLooping Through All Key-Value Pairs")
+    favourite_car = {
+        'Jay' : 'Benz',
+        'Jane': 'bmw',
+        'Jason' : 'Audi'
+    }
+    for name, car in favourite_car.items():
+        print(car.title() + " " + "is" + " " +
+              name.title() + "'s favourite car")
+    
+loopingthroughkeyvalue()
+
+def loopingthroughkeys():
+    print("\nLooping through allthe Keys in a dictionary")
     

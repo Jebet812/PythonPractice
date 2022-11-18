3b4f50fdf272a026c48123113182e7170218400d
Jebet812 <cynthiajebet08@gmail.com>
2022-11-18 08:58:17 +0300

    Modified

--- PYTHON/Classes.py
+++ PYTHON/Classes.py
@@ -30,3 +30,21 @@ my_baby = Baby('Sandra', 2)
 print("My baby's name is " + my_baby.name.title())
 print(my_baby.name.title() + " is " + str(my_baby.age) + " years old")
     
+print("\nCalling Methods")
+
+class Baby():
+    
+     def __init__(self, name, age):
+        """Initialize name and age attributes"""
+        self.name = name
+        self.age = age
+        
+     def sit(self):
+    
+          
+ my_baby = Baby('Sandra', 2)
+
+print("My baby's name is " + my_baby.name.title() + 
+      ". She loves " + my_baby.sit)
+print(my_baby.name.title() + " is " + str(my_baby.age) + " years old."
+      "She rarely " + my_baby.cry)

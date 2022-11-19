500b270ab403e49be83febe0ed12b92d6109783b
Jebet812 <cynthiajebet08@gmail.com>
2022-11-19 14:20:59 +0300

    Modified

--- PYTHON/Python Beginner Projects/Madlib.py
+++ PYTHON/Python Beginner Projects/Madlib.py
@@ -1,6 +1,13 @@
 # The project helps one familiarize themselves with working with f strings
 # and general string concatenation in Python.
 
-Colour = input("Colour: ")
+name= input('Name: ')
+colour = input("Colour: ")
+store = input("Store: ")
+price = input("Price: ")
 
-madlib = 
+madlib = f"My name is {name}. I tend to wear dresses that are colour {colour}.\
+I buy most of my dresses in  {store}. The prices cost around {price} dollars"
+
+print(madlib)
+    

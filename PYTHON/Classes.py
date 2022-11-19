6068ca60949c4076b82925ae306e33d98075f0a4
Jebet812 <cynthiajebet08@gmail.com>
2022-11-19 10:09:06 +0300

    Modified

--- PYTHON/Classes.py
+++ PYTHON/Classes.py
@@ -136,42 +136,8 @@ print ("My new house will be made of a " + my_house.get_a_house())
 my_house.update_floors(4)
 my_house.floors()
 
-print("\nIncrementing an Attribute's Value Through a Method")
+print("Inheritance")
 
-class House():
-    # Building a House
-    
-    def __init__(self, door, window, stair):
-        # Initialize attributes of building a house
-        self.door = door
-        self.window = window
-        self.stair = stair
-        self.floor = 2
-        
-    def get_a_house(self):
-        # Return a descriptive house in the method
-        requirements = self.door + ', a '+ self.window + ' and a ' + self.stair
-        return requirements
-    
-    def floors(self):
-        print("I only want " + str(self.floor) + " floors in the house.")
-        
-    
-    def update_floors(self, story):
-        self.floor = story
-        
-    def add_stairs (self, another_stair):
-        self.stair += another_stair
-        print("The stair I am adding is")   
-        
-          
-my_house = House('Panel door', 'Casement window', 'Spiral staircase')
-print ("My new house will be made of a " + my_house.get_a_house())
 
-my_house.update_floors(4)
-my_house.floors()
-
-my_house.add_stairs(2)
-my_house.stair()
 
 

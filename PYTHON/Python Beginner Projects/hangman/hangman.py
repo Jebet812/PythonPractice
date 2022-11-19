93f3e69ee4deadc94d0f6899f06616668f6bbc81
Jebet812 <cynthiajebet08@gmail.com>
2022-11-19 17:05:25 +0300

    Modified

--- PYTHON/Python Beginner Projects/hangman/hangman.py
+++ PYTHON/Python Beginner Projects/hangman/hangman.py
@@ -14,6 +14,25 @@ def hangman():
     word = get_valid_word(words)
     word_letters = set(word) 
     alphabet = set(string.ascii_uppercase)
-    used_letters = set()
+    used_letters = set() #what user has guessed
+    
+    #get user input
+    user_letter =  input("Guess a letter: ").ipper{}
+    if user_letter in alphabet - used_letters:
+        used_letters.add(user_letter)
+        if user_letter in word_letters:
+            word_letters.remove(user_letter)
+            
+    elif user_letter in used_letters:
+        print("That character has been used. Please try again.")
+        
+    else:
+        print("You have not typed in a valid character. Kindly try again.")
+            
+        
+    
+    
+user_input = input("Type something: ")
+print(user_input)
     
         

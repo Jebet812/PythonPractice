9c054d98f0afa7e056b9c049e448fcde40070d08
Jebet812 <cynthiajebet08@gmail.com>
2022-11-19 16:24:47 +0300

    Modified

--- PYTHON/Python Beginner Projects/hangman/hangman.py
+++ PYTHON/Python Beginner Projects/hangman/hangman.py
@@ -2,3 +2,14 @@ import random
 from words import words
 
 
+def get_valid_word(words):
+    word = random.choice(words)  #this will choose a random list from the list
+    while '-' in word or ' ' in word:
+        word = random.choice(words)
+        
+        
+    return(word)
+
+def hangman():
+    
+        

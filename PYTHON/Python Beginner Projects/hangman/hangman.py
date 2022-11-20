f94ec98260b89e468eebde6fb03a5ed1dc07f8f2
Jebet812 <cynthiajebet08@gmail.com>
2022-11-20 11:06:56 +0300

    Modified

--- PYTHON/Python Beginner Projects/hangman/hangman.py
+++ PYTHON/Python Beginner Projects/hangman/hangman.py
@@ -17,21 +17,30 @@ def hangman():
     used_letters = set() #what user has guessed
     
     #get user input
-    user_letter =  input("Guess a letter: ").upper()
-    if user_letter in alphabet - used_letters:
-        used_letters.add(user_letter)
-        if user_letter in word_letters:
-            word_letters.remove(user_letter)
-            
-    elif user_letter in used_letters:
-        print("That character has been used. Please try again.")
+    while len(word_letters) > 0:
+        # letters used
+        # ' '.join({'a', cd,}) --> 'a, cd'
+        print("You have guessed these letters: ", ' '.join(used_letters))
         
-    else:
-        print("You have not typed in a valid character. Kindly try again.")
+        # what current word is with dashes where unguessed letters are (W - R -D)
+        word_list = [letter if letter in used_letters else  '-' for letter in word]
+        print("Current word: ", ' '.join(word_list))
+        
+        
+        user_letter =  input("Guess a letter: ").upper()
+        if user_letter in alphabet - used_letters:
+            used_letters.add(user_letter)
+            if user_letter in word_letters:
+                word_letters.remove(user_letter)
+            
+        elif user_letter in used_letters:
+            print("That character has been used. Please try again.")
+        
+        else:
+            print("You have not typed in a valid character. Kindly try again.")
             
         
-    
-    
+    #gets here when len(word_letters) == 0   
 user_input = input("Type something: ")
 print(user_input)
     
